b4295b5229c7eb53f699e82a88ffb2e0e26190b4
ftang1996 <ftang1996@gmail.com>
2018-07-28 19:36:26 -0700

    DB initial
    Nonprofit and Item
    Nonprofit populated only with name=Saint John’s

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,22 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
 
 # import os, logging
 # from logging.handlers import RotatingFileHandler
-# from flask import Flask
-# from config import Config
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
+
+
 # from flask_login import LoginManager
 #
-# app = Flask(__name__)
-# app.config.from_object(Config)
-# db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
+\
 #
 #
 #
